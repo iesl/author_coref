23bd86314d49743865ca10b875285a6f6a247e30
Nicholas Monath <nicholas.monath@gmail.com>
2016-04-11 16:32:59 -0400

    config

--- scripts/coref/run_coref.sh
+++ scripts/coref/run_coref.sh
@@ -5,7 +5,7 @@ jarpath="target/author_coref-1.1-SNAPSHOT-jar-with-dependencies.jar"
 
 
 settingsConf=${1:-config/coref/ParallelCoref.config}
-weightsConf=${2:-config/coref/DefaultWeightsWithoutTopicsAndKeywords.config}
+weightsConf=${2:-config/coref/WeightsToBeTuned.config}
 
 time java -Xmx30G -cp $jarpath edu.umass.cs.iesl.author_coref.coreference.RunParallelCoreference \
 $(cat $settingsConf) \
